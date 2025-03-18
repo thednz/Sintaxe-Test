ea093ffce5f643d10fd7a41bc7a9dd638da1fdab
thednz <augustinpro90@gmail.com>
2025-03-17 22:36:28 -0300

    :tada: Commit | Inicial kapa kapa kapa

---
+++ src/mainx/Main.java
@@ -0,0 +1,14 @@
+package mainx;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number = 1;
+		String name = "LAe ele";
+		System.out.println("Hello World!!!");
+		System.out.println("This menssage show for you,\ncongratulations " + name +  number);
+		
+		
+	}
+
+}
