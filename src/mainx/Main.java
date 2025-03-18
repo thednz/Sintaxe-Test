71015307c7979707830c28b3bc802251c3fe7801
thednz <augustinpro90@gmail.com>
2025-03-17 22:51:46 -0300

    seconda commit

--- src/mainx/Main.java
+++ src/mainx/Main.java
@@ -3,10 +3,23 @@ package mainx;
 public class Main {
 
 	public static void main(String[] args) {
-		int number = 1;
-		String name = "LAe ele";
-		System.out.println("Hello World!!!");
-		System.out.println("This menssage show for you,\ncongratulations " + name +  number);
+		int a = 4;
+		boolean b = a == 5;
+		
+		System.out.println("correct this afirmative "+ b);
+		System.out.println("correct this afirmative "+ !b);
+		
+		if (a == 5) {
+			System.out.println("around want to going ");
+		}else {
+			System.out.println("o baby "+ a++);
+		}
+		
+		if (a == 4) {
+			System.out.println("around want to going ");
+		}else {
+			System.out.println("o baby "+ ++a);
+		}
 		
 		
 	}
